181e57b0b01c45dee2c5c0411e059da90aea41b2
Atushi Kawamura <atsushi1230.sdi@gmail.com>
2017-02-08 11:55:00 +0900

    あつし

--- app/src/main/java/com/example/fujinohiroki/homey/MainActivity.java
+++ app/src/main/java/com/example/fujinohiroki/homey/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.fujinohiroki.homey;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    TextView textView2;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    int i = 0;
+    EditText editText;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("", "onCreate()");
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
+        textView5 = (TextView) findViewById(R.id.textView5);
+        textView6 = (TextView) findViewById(R.id.textView6);
+        editText = (EditText) findViewById(R.id.edit_text);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // エディットテキストのテキストを取得
+                String text = editText.getText().toString();
+                // 入力した文字列の削除
+                editText.getEditableText().clear();
+                if(i==0) {
+                    // 取得したテキストを TextView に張り付ける
+                    textView.setText(text);
+                    textView2.setText("Hello");
+                }else if(i==1) {
+                    textView3.setText(text);
+                    textView4.setText("Goodbye");
+                }
+                else if(i==2){
+                    textView5.setText(text);
+                    textView6.setText("Nice to meet you");
+                }
+                else{
+                }
+                i++;
+            }
+        });
     }
 }
