da858b7168899ffcb3efb2d4bcc15c07ee86f9ae
yanagawa <yanagawa@fringe81.com>
2017-03-03 20:56:19 +0900

    user class created

--- app/src/main/java/com/example/fujinohiroki/homey/ChatMessageAdapter.java
+++ app/src/main/java/com/example/fujinohiroki/homey/ChatMessageAdapter.java
@@ -77,3 +77,5 @@ public class ChatMessageAdapter extends RealmBaseAdapter<ChatMessage> {
         return convertView;
     }
 }
+
+
---
+++ app/src/main/java/com/example/fujinohiroki/homey/User.java
@@ -0,0 +1,47 @@
+package com.example.fujinohiroki.homey;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by yanagawa on 2017/03/03.
+ */
+
+public class User extends RealmObject{
+    private String          name;
+    private long            id;
+    private String          pass;
+    private String          email;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return (int) id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
